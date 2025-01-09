284d70a5ef1257cc14b5868f8ce9430a0c242672
fkurusu7 <coding.fcv@gmail.com>
2025-01-08 23:46:40 -0600

    add work experience and education layout

--- src/features/resume/Resume.tsx
+++ src/features/resume/Resume.tsx
@@ -1,4 +1,20 @@
-import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineMail, AiOutlineX } from 'react-icons/ai';
+import {
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+  AiOutlineX,
+  AiOutlineHtml5,
+  AiOutlineJavaScript,
+  AiOutlineJava,
+  AiOutlinePython,
+  AiOutlineRead,
+} from 'react-icons/ai';
+import { FaNodeJs, FaReact, FaDocker, FaCss3 } from 'react-icons/fa';
+import { GiWeightLiftingUp } from 'react-icons/gi';
+import { HiOutlineCodeBracket, HiOutlineFlag } from 'react-icons/hi2';
+import { MdOutlineDirectionsBike } from 'react-icons/md';
+import { PiPersonSimpleHike } from 'react-icons/pi';
+import { VscVscodeInsiders } from 'react-icons/vsc';
 
 function Resume() {
   return (
@@ -23,16 +39,16 @@ function Resume() {
           />
         </div>
 
-        <aside className="resume__options">
-          <div className="resume__options-option">
-            <h3 className="resume__options-heading">Contact Info</h3>
+        <aside className="resume__background">
+          <div className="resume__background-option">
+            <h3 className="resume__background-heading">Contact Info</h3>
             <ul>
               <li>
                 <a
                   href="mailto:your.email@example.com"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="resume__options-link"
+                  className="resume__background-link"
                 >
                   <AiOutlineMail /> <span>Email</span>
                 </a>
@@ -42,7 +58,7 @@ function Resume() {
                   href="https://github.com/fkurusu7"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="resume__options-link"
+                  className="resume__background-link"
                 >
                   <AiOutlineGithub /> <span>Github</span>
                 </a>
@@ -52,7 +68,7 @@ function Resume() {
                   href="#"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="resume__options-link"
+                  className="resume__background-link"
                 >
                   <AiOutlineLinkedin /> <span>Linkedin</span>
                 </a>
@@ -62,41 +78,194 @@ function Resume() {
                   href="https://x.com/FerCuVa"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="resume__options-link"
+                  className="resume__background-link"
                 >
                   <AiOutlineX /> <span>X</span>
                 </a>
               </li>
             </ul>
           </div>
-          <div className="resume__options-option">
-            <h3 className="resume__options-heading">Stack</h3>
+          <div className="resume__background-option">
+            <h3 className="resume__background-heading">Stack</h3>
             <ul>
-              <li>React.js</li>
-              <li>Node.js</li>
-              <li>VsCode</li>
-              <li>Docker</li>
+              <li className="resume__background-stack">
+                <FaReact />
+                <span>React.js</span>
+              </li>
+              <li className="resume__background-stack">
+                <FaNodeJs />
+                <span>Node.js</span>
+              </li>
+              <li className="resume__background-stack">
+                <VscVscodeInsiders />
+                <span>VsCode</span>
+              </li>
+              <li className="resume__background-stack">
+                <FaDocker />
+                <span>Docker</span>
+              </li>
             </ul>
           </div>
-          <div className="resume__options-option">
-            <h3 className="resume__options-heading">Languages</h3>
+          <div className="resume__background-option">
+            <h3 className="resume__background-heading">Languages</h3>
             <ul>
-              <li>English</li>
-              <li>Spanish</li>
+              <li className="resume__background-langs">
+                <HiOutlineFlag />
+                <span>English</span>
+              </li>
+              <li className="resume__background-langs">
+                <HiOutlineFlag />
+                <span>Spanish</span>
+              </li>
+              <li className="resume__background-langs">
+                <AiOutlineHtml5 />
+                <span>HTML</span>
+              </li>
+              <li className="resume__background-langs">
+                <FaCss3 />
+                <span>CSS</span>
+              </li>
+              <li className="resume__background-langs">
+                <AiOutlineJavaScript />
+                <span>Javascript</span>
+              </li>
+              <li className="resume__background-langs">
+                <AiOutlinePython />
+                <span>Python</span>
+              </li>
+              <li className="resume__background-langs">
+                <AiOutlineJava />
+                <span>Java</span>
+              </li>
             </ul>
           </div>
-          <div className="resume__options-option">
-            <h3 className="resume__options-heading">Interests</h3>
+          <div className="resume__background-option">
+            <h3 className="resume__background-heading">Interests</h3>
             <ul>
-              <li>Biking</li>
-              <li>Reading</li>
-              <li>Calisthenics</li>
-              <li>Coding</li>
+              <li className="resume__background-langs">
+                <HiOutlineCodeBracket />
+                <span>Coding</span>
+              </li>
+              <li className="resume__background-langs">
+                <MdOutlineDirectionsBike />
+                <span>Biking</span>
+              </li>
+              <li className="resume__background-langs">
+                <AiOutlineRead />
+                <span>Reading</span>
+              </li>
+              <li className="resume__background-langs">
+                <GiWeightLiftingUp />
+                <span>Calisthenics</span>
+              </li>
+              <li className="resume__background-langs">
+                <PiPersonSimpleHike />
+                <span>Hiking</span>
+              </li>
             </ul>
           </div>
         </aside>
 
-        <main className="resume__experience">Experience</main>
+        <main className="resume__experience">
+          <div className="resume__experience-container">
+            <h2>Work Experience</h2>
+            <section className="resume__experience-jobs">
+              <section className="resume__experience-job">
+                <article className="resume__experience-where">
+                  <h3>Freelance</h3>
+                  <i>Mexico</i>
+                  <p>
+                    Jan, 2023 <span>-</span> Present
+                  </p>
+                </article>
+                <article className="resume__experience-description">
+                  <h3>Web Developer (Full-Stack)</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis quaerat
+                    dolorum itaque consequuntur voluptas. Necessitatibus velit eius, quam
+                    blanditiis, modi consequatur vero deserunt sapiente iusto non debitis? Labore,
+                    totam.
+                  </p>
+                </article>
+              </section>
+              <section className="resume__experience-job">
+                <article className="resume__experience-where">
+                  <h3>Morgan Stanley</h3>
+                  <i>New York, USA</i>
+                  <p>
+                    Sep, 2019 <span>-</span> Jul, 2022
+                  </p>
+                </article>
+                <article className="resume__experience-description">
+                  <h3>Java Tech Lead</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis quaerat
+                    dolorum itaque consequuntur voluptas. Necessitatibus velit eius, quam
+                    blanditiis, modi consequatur vero deserunt sapiente iusto non debitis? Labore,
+                    totam.
+                  </p>
+                </article>
+              </section>
+              <section className="resume__experience-job">
+                <article className="resume__experience-where">
+                  <h3>Morgan Stanley</h3>
+                  <i>New York, USA</i>
+                  <p>
+                    Sep, 2019 <span>-</span> Jul, 2022
+                  </p>
+                </article>
+                <article className="resume__experience-description">
+                  <h3>Java Tech Lead</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis quaerat
+                    dolorum itaque consequuntur voluptas. Necessitatibus velit eius, quam
+                    blanditiis, modi consequatur vero deserunt sapiente iusto non debitis? Labore,
+                    totam.
+                  </p>
+                </article>
+              </section>
+              <section className="resume__experience-job">
+                <article className="resume__experience-where">
+                  <h3>Morgan Stanley</h3>
+                  <i>New York, USA</i>
+                  <p>
+                    Sep, 2019 <span>-</span> Jul, 2022
+                  </p>
+                </article>
+                <article className="resume__experience-description">
+                  <h3>Java Tech Lead</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis quaerat
+                    dolorum itaque consequuntur voluptas. Necessitatibus velit eius, quam
+                    blanditiis, modi consequatur vero deserunt sapiente iusto non debitis? Labore,
+                    totam.
+                  </p>
+                </article>
+              </section>
+            </section>
+          </div>
+          <div className="resume__experience-container">
+            <h2>Education</h2>
+            <section className="resume__experience-education">
+              <section className="resume__experience-school">
+                <article className="resume__experience-where">
+                  <h3>UNITEC</h3>
+                  <i>Mexico City, Mexico</i>
+                  <p>
+                    Aug, 2004 <span>-</span> Aug, 2007
+                  </p>
+                </article>
+                <article className="resume__experience-description">
+                  <h3>Bachelor's Degree in Management Information Systems (MIS)</h3>
+                  <p>
+                    Information Technology, Business Administration, Systems Analysis, Database
+                    Management, Business Process Management
+                  </p>
+                </article>
+              </section>
+            </section>
+          </div>
+        </main>
       </div>
     </div>
   );

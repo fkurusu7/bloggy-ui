1af2ea0378a1f22bebe88d3f3b2a03eee18eacce
fkurusu7 <coding.fcv@gmail.com>
2025-01-12 23:06:15 -0600

    add error handling to input fields

--- src/features/auth/UserAuth.tsx
+++ src/features/auth/UserAuth.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import { z } from 'zod';
 import {
   HiOutlineEnvelope,
   HiOutlineEye,
@@ -5,8 +7,9 @@ import {
   HiOutlineIdentification,
   HiOutlineKey,
 } from 'react-icons/hi2';
+
 import Button from '../../component/Button';
-import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type UserAuthProps = {
   type: 'signin' | 'signup';
@@ -18,22 +21,84 @@ type UserFormData = {
   fullname?: string;
 };
 
+// Define the error message type
+type FormError = {
+  field: string;
+  message: string;
+};
+
+// Validate Fields with Zod
+const signupSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .regex(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/,
+      'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+    ),
+  fullname: z.string().min(3, 'Full name must be at least 3 characters'),
+});
+
+const signinSchema = signupSchema.omit({ fullname: true });
+
 function UserAuth({ type }: UserAuthProps) {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState<UserFormData>({
     email: '',
     password: '',
-    ...(type === 'signin' && { fullname: '' }),
+    ...(type !== 'signin' && { fullname: '' }),
   });
-
   const [showPassword, setShowPassword] = useState(false);
+  const [errorsMessage, setErrorsMessage] = useState<FormError[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     const { value, id } = ev.target;
     setFormData((prevData) => ({ ...prevData, [id]: value }));
   };
 
-  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
+    try {
+      setIsLoading(true);
+      // Validate Fields
+      const schema = type === 'signin' ? signinSchema : signupSchema;
+      const result = schema.parse(formData);
+      setErrorsMessage([]);
+
+      // generate auth path ==> /signin or /signup
+      const path = type === 'signin' ? 'signin' : 'signup';
+      const response = await fetch(`/api/auth/${path}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(result),
+      });
+
+      const jsonData = await response.json();
+      console.log(jsonData);
+      if (response.ok) {
+        if (type === 'signin') {
+          navigate('/blog');
+        } else {
+          navigate('/signin');
+        }
+      }
+
+      // if ok navigate to /blog if signin or /signin if signup
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errors = error.errors.map((e) => ({
+          field: e.path.join('.'),
+          message: e.message,
+        }));
+        setErrorsMessage(errors);
+        console.log(errorsMessage);
+      }
+    } finally {
+      setFormData({ email: '', password: '', fullname: '' });
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -63,9 +128,17 @@ function UserAuth({ type }: UserAuthProps) {
               placeholder="placeholder@test.com"
               value={formData.email}
               onChange={handleChange}
-              className="auth__input-box"
+              className={`auth__input-box ${errorsMessage.some((error) => error.field === 'email') ? 'auth__input-box--error' : ''}`}
             />
             <HiOutlineEnvelope className="auth__input-icon" />
+            {errorsMessage.map(
+              (error) =>
+                error.field === 'email' && (
+                  <span key={error.field} className="auth__input--error">
+                    {error.message}
+                  </span>
+                )
+            )}
           </div>
           <div className="auth__input-container">
             <input
@@ -77,9 +150,7 @@ function UserAuth({ type }: UserAuthProps) {
               onChange={handleChange}
               className="auth__input-box"
             />
-            {/* TODO: add icon and handle password visibility */}
             <HiOutlineKey className="auth__input-icon" />
-
             <HiOutlineEyeSlash
               className={`auth__input-icon auth__input-icon--password ${showPassword ? 'auth__input-icon--password--hidden' : ''}`}
               onClick={() => setShowPassword(!showPassword)}
@@ -90,7 +161,7 @@ function UserAuth({ type }: UserAuthProps) {
             />
           </div>
           <div className="auth__button-container">
-            <Button type="submit" size="small" variant="form">
+            <Button type="submit" size="small" variant="form" isLoading={isLoading}>
               {type === 'signin' ? 'Sign in' : 'Sign up'}
             </Button>
           </div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5174",
+        secure: false,
+      },
+    },
+  },
   plugins: [react(), eslint()],
 });

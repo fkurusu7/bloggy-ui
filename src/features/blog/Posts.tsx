6c972e2dfa9227ea122474aa55e079b74143a730
fkurusu7 <coding.fcv@gmail.com>
2025-01-16 17:30:47 -0600

    add buttons for edit and remove the post in the blog page

--- src/features/blog/Posts.tsx
+++ src/features/blog/Posts.tsx
@@ -1,74 +1,75 @@
 import { HiOutlinePencilSquare, HiOutlineTrash } from 'react-icons/hi2';
 import Button from '../../component/Button';
+import { useAppSelector } from '../../context/useContextTypes';
+import { Link } from 'react-router-dom';
 
 function Posts() {
   // useEffect to fetchPosts
+  const { currentUser } = useAppSelector((state) => state.user);
 
   return (
     // Map thorugh Posts!
     <section className="blog__main-posts">
-      <div className="blog__main-post">
+      <Link to="" className="blog__main-post">
         <div className="blog__main-post-heading">
-          <h2>Post</h2> <span>Date</span>
+          <h2>Post</h2> <span>Jun, 23</span>
         </div>
         <p className="blog__main-post-description">
           Lorem ipsum, dolopellat natus enim, illum obcaecati voluptatibus iste. Lorem ipsum dolor
           sit amet consectetur adipisicing elit. Quibusdam animi id perferendis, magni blanditiis
           quos.
         </p>
-        <div className="blog__main-post-actions">
-          <Button variant="icon">
-            <HiOutlineTrash color="var(--color-red-700)" />
-          </Button>
-          <Button variant="icon">
-            <HiOutlinePencilSquare color="var(--color-green-700)" />
-          </Button>
-        </div>
-      </div>
-      <div className="blog__main-post">
+        {currentUser && (
+          <div className="blog__main-post-actions">
+            <Button variant="icon">
+              <HiOutlineTrash color="var(--color-red-700)" />
+            </Button>
+            <Button variant="icon">
+              <HiOutlinePencilSquare color="var(--color-green-700)" />
+            </Button>
+          </div>
+        )}
+      </Link>
+      <Link to="" className="blog__main-post">
         <div className="blog__main-post-heading">
-          <h2>Post</h2> <span>Date</span>
+          <h2>Post</h2> <span>Jun, 23</span>
         </div>
         <p className="blog__main-post-description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, temporibus assumenda quam
-          magni corporis harum vitae vel laudantium ex numquam omnis itaque mollitia nesciunt ut a
-          recusandae sunt fugit ne. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-          quos quo praesentium temporibus. Laborum odit deserunt pariatur neque accusantium iure.
+          Lorem ipsum, dolopellat natus enim, illum obcaecati voluptatibus iste. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Quibusdam animi id perferendis, magni blanditiis
+          quos.
         </p>
-      </div>
-      <div className="blog__main-post">
+        {currentUser && (
+          <div className="blog__main-post-actions">
+            <Button variant="icon">
+              <HiOutlineTrash color="var(--color-red-700)" />
+            </Button>
+            <Button variant="icon">
+              <HiOutlinePencilSquare color="var(--color-green-700)" />
+            </Button>
+          </div>
+        )}
+      </Link>
+      <Link to="" className="blog__main-post">
         <div className="blog__main-post-heading">
-          <h2>Post</h2> <span>Date</span>
+          <h2>Post</h2> <span>Jun, 23</span>
         </div>
         <p className="blog__main-post-description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, temporibus assumenda quam
-          magni corporis harum vitae vel laudantium ex numquam omnis itaque mollitia nesciunt ut a
-          recusandae sunt fugit ne. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-          quos quo praesentium temporibus. Laborum odit deserunt pariatur neque accusantium iure.
+          Lorem ipsum, dolopellat natus enim, illum obcaecati voluptatibus iste. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Quibusdam animi id perferendis, magni blanditiis
+          quos.
         </p>
-      </div>
-      <div className="blog__main-post">
-        <div className="blog__main-post-heading">
-          <h2>Post</h2> <span>Date</span>
-        </div>
-        <p className="blog__main-post-description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, temporibus assumenda quam
-          magni corporis harum vitae vel laudantium ex numquam omnis itaque mollitia nesciunt ut a
-          recusandae sunt fugit ne. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-          quos quo praesentium temporibus. Laborum odit deserunt pariatur neque accusantium iure.
-        </p>
-      </div>
-      <div className="blog__main-post">
-        <div className="blog__main-post-heading">
-          <h2>Post</h2> <span>Date</span>
-        </div>
-        <p className="blog__main-post-description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, temporibus assumenda quam
-          magni corporis harum vitae vel laudantium ex numquam omnis itaque mollitia nesciunt ut a
-          recusandae sunt fugit ne. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-          quos quo praesentium temporibus. Laborum odit deserunt pariatur neque accusantium iure.
-        </p>
-      </div>
+        {currentUser && (
+          <div className="blog__main-post-actions">
+            <Button variant="icon">
+              <HiOutlineTrash color="var(--color-red-700)" />
+            </Button>
+            <Button variant="icon">
+              <HiOutlinePencilSquare color="var(--color-green-700)" />
+            </Button>
+          </div>
+        )}
+      </Link>
     </section>
   );
 }

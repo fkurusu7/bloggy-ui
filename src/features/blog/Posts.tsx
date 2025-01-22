444e7eacb370f285fb9e75d89598b78c23e780b3
fkurusu7 <coding.fcv@gmail.com>
2025-01-21 19:44:02 -0600

    fix commit errors

--- src/features/blog/Posts.tsx
+++ src/features/blog/Posts.tsx
@@ -2,74 +2,52 @@ import { HiOutlinePencilSquare, HiOutlineTrash } from 'react-icons/hi2';
 import Button from '../../component/Button';
 import { useAppSelector } from '../../context/useContextTypes';
 import { Link, useParams } from 'react-router-dom';
-
 import { useBlogPosts } from '../../hooks/useBlogPosts';
 import { formatShortDate } from '../../utils/helpers';
 
 function Posts() {
   const { currentUser } = useAppSelector((state) => state.user);
   const { searchTerm, tag } = useParams();
-
   const { posts, isLoadingPosts, error, getTitle } = useBlogPosts({ searchTerm, tag });
 
   return (
-    // Map thorugh Posts!
     <>
-      <h2 className="blog__main-title">{getTitle()}</h2>{' '}
-<<<<<<< HEAD
-      <section className="blog__main-posts">
-        {isLoadingPosts ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>{error}</p>
-        ) : (
-          posts.map((post: any) => {
-=======
-      {/* Title Will be passed when loading posts or search term or * */}
+      {/* Title Will be passed when loading posts or search term or any */}
+      <h2 className="blog__main-title">{getTitle()}</h2>
       {isLoadingPosts ? (
         <p>Loading...</p>
       ) : error ? (
         <p>{error}</p>
       ) : (
         <section className="blog__main-posts">
-          {posts.map((post) => {
->>>>>>> admin_blog
-            return (
-              <Link
-                to={`/blog/posts/${post.slug}`}
-                className="blog__main-posts-post"
-                key={post.slug}
-              >
-                <div className="blog__main-posts-post-heading">
-                  <h2>{post.title}</h2> <span>{formatShortDate(post.createdAt)}</span>
+          {posts.map((post) => (
+            <Link to={`/blog/posts/${post.slug}`} className="blog__main-posts-post" key={post.slug}>
+              <div className="blog__main-posts-post-heading">
+                <h2>{post.title}</h2>
+                <span>{formatShortDate(post.createdAt)}</span>
+              </div>
+              <p className="blog__main-posts-post-description">{post.description}</p>
+              <div className="blog__main-tags">
+                {post.tags.map((tag) => (
+                  <p key={tag.slug} className="blog__main-tag">
+                    {tag.name}
+                  </p>
+                ))}
+              </div>
+              {currentUser && (
+                <div className="blog__main-posts-post-actions">
+                  <Button variant="icon">
+                    <HiOutlineTrash color="var(--color-red-700)" />
+                  </Button>
+                  <Button variant="icon">
+                    <HiOutlinePencilSquare color="var(--color-green-700)" />
+                  </Button>
                 </div>
-                <p className="blog__main-posts-post-description">{post.description}</p>
-
-                <div className="blog__main-tags">
-                  {post.tags.map((tag: any) => {
-                    return (
-                      <p key={tag.slug} className="blog__main-tag">
-                        {tag.name}
-                      </p>
-                    );
-                  })}
-                </div>
-
-                {currentUser && (
-                  <div className="blog__main-posts-post-actions">
-                    <Button variant="icon">
-                      <HiOutlineTrash color="var(--color-red-700)" />
-                    </Button>
-                    <Button variant="icon">
-                      <HiOutlinePencilSquare color="var(--color-green-700)" />
-                    </Button>
-                  </div>
-                )}
-              </Link>
-            );
-          })
-        )}
-      </section>
+              )}
+            </Link>
+          ))}
+        </section>
+      )}
     </>
   );
 }

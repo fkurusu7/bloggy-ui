6c078ff97e8966cfdff1622e241f043bb071f1f8
fkurusu7 <coding.fcv@gmail.com>
2025-01-24 16:20:16 -0600

    upload image to S3

--- src/features/blog_admin/CreatePost.tsx
+++ src/features/blog_admin/CreatePost.tsx
@@ -1,6 +1,71 @@
+import React, { useState } from 'react';
+import { z } from 'zod';
 import Button from '../../component/Button';
+import toast from 'react-hot-toast';
+import { uploadImageToAWS } from '../../utils/aws';
+import { HiOutlinePhoto } from 'react-icons/hi2';
+
+// const postInitialState = {
+//   title: '',
+//   banner: '',
+//   content: [],
+//   tags: [],
+//   description: '',
+// };
+
+const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/jpg'];
+const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
+
+// Validate Editor Fields
+const imageSchema = z.object({
+  image: z
+    .instanceof(File)
+    .refine((file) => file?.size <= MAX_IMAGE_SIZE, 'Max image size is 5MB')
+    .refine((file) => ALLOWED_IMAGE_TYPES.includes(file?.type)),
+});
 
 function CreatePost() {
+  const [isUploadingImage, setIsUploadingImage] = useState(false);
+  const postInitialState = {
+    title: '',
+    banner: '',
+    content: [],
+    tags: [],
+    description: '',
+  };
+  const [editorFormData, setEditorFormData] = useState(postInitialState);
+
+  const handleBannerImgUpload = async (ev: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      setIsUploadingImage(true);
+      if (ev.target.files && ev.target.files.length > 0) {
+        const img = ev.target.files[0];
+        const imageValidation = imageSchema.parse({ image: img });
+        console.log(imageValidation);
+        // toast.success('Valid image');
+        // Upload Image to AWS S3
+        const imageUploadedURL = await uploadImageToAWS(img);
+        console.log(imageUploadedURL);
+
+        setEditorFormData((prevData) => ({
+          ...prevData,
+          banner: imageUploadedURL,
+        }));
+        toast.success('Image uploaded successfully');
+      }
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        toast.error(error.errors.map((e) => e.message).join(', '));
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setIsUploadingImage(false);
+    }
+  };
+
+  console.log(editorFormData);
+
   return (
     <div className="create__container">
       <h2 className="heading-1">Create a Post</h2>
@@ -11,11 +76,27 @@ function CreatePost() {
           <div className="create__main-banner">
             <label htmlFor="banner" className="create__main-banner-label">
               {/* loading image */}
-              <div className="lab">Loading image</div>
+              {isUploadingImage && <div className="lab">Loading image</div>}
               {/* image to show when uploaded */}
-              <img src="/write_sth.jpg" alt="Banner post image" />
+              {editorFormData.banner ? (
+                <img
+                  src={editorFormData.banner || '/write_sth.jpg'}
+                  alt="Banner post image"
+                  style={isUploadingImage ? { opacity: 0.3 } : {}}
+                />
+              ) : (
+                <HiOutlinePhoto />
+              )}
               {/* input to upload */}
-              <input hidden type="file" name="banner" id="banner" accept=".png, .jpg, .jpeg" />
+              <input
+                hidden
+                type="file"
+                name="banner"
+                id="banner"
+                accept=".png, .jpg, .jpeg"
+                onChange={handleBannerImgUpload}
+                disabled={isUploadingImage}
+              />
             </label>
           </div>
           {/* TITLE */}
---
+++ src/utils/aws.tsx
@@ -0,0 +1,35 @@
+export const uploadImageToAWS = async (image: File) => {
+  // Validate file type and size
+  if (!image.type.includes('image/')) {
+    throw new Error('Please upload an image file');
+  }
+  // 5mb limit
+  if (image.size > 5 * 1024 * 1024) {
+    throw new Error('Image size should be less than 5MB');
+  }
+
+  // Fetch AWS-S3 signed URL to upload image from frontend
+  const response = await fetch('/api/blog/getImageUploadUrl');
+  if (!response.ok) {
+    throw new Error('Failed to get upload URL');
+  }
+
+  const { urlUploadImage } = await response.json();
+
+  // Upload to S3
+  const awsResponse = await fetch(urlUploadImage, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': image.type,
+    },
+    body: image,
+  });
+
+  if (!awsResponse.ok) {
+    throw new Error('Failed to upload image to AWS-S3');
+  }
+
+  // return the public URL
+  console.log('AWS url: ', urlUploadImage);
+  return urlUploadImage.split('?')[0];
+};

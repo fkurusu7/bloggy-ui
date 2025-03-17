8ac505c47adb74b1ebf7cebdb40cf352f19bb3ba
fkurusu7 <coding.fcv@gmail.com>
2025-03-17 16:14:07 -0600

    add credentials for production

--- src/features/auth/UserAuth.tsx
+++ src/features/auth/UserAuth.tsx
@@ -135,6 +135,7 @@ function UserAuth({ type }: UserAuthProps) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(result),
+        credentials: 'include',
       });
 
       const jsonData = await response.json();

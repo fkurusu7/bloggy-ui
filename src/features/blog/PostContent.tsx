20112753d8f6e4f3a73a5ab0887f977283791e9b
fkurusu7 <coding.fcv@gmail.com>
2025-03-24 23:02:22 -0600

    add styles for code blocks

--- src/features/blog/PostContent.tsx
+++ src/features/blog/PostContent.tsx
@@ -1,21 +1,59 @@
 import { useEffect } from 'react';
-import Prism from 'prismjs';
-import DOMPurify from 'dompurify';
 
+import DOMPurify from 'dompurify';
+import { logger } from '../../utils/helpers';
+
+// Core
+import Prism from 'prismjs';
+// Theme
+// import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/themes/prism-okaidia.css';
+// import 'prismjs/themes/prism-solarizedlight.css'; // Light Mode
+// import 'prismjs/themes/prism-solarizeddark.css'; // Dark Mode - Not working
+
+// dependencies
+import 'prismjs/components/prism-core';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-clike'; // Required before JavaScript
+
+// Language packs
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-sql';
+/* 
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-diff';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-rust';
+ */
 type PostContentProps = {
   content: string; // This says "this box can only hold text!"
 };
 
 function PostContent({ content }: PostContentProps) {
   useEffect(() => {
-    Prism.highlightAll();
+    setTimeout(() => {
+      Prism.highlightAll(); // Ensures Prism runs after the DOM updates
+    }, 0);
   }, [content]);
 
+  const cleanContent = DOMPurify.sanitize(content, {
+    ADD_TAGS: ['code'],
+    ADD_ATTR: ['class'],
+  });
+  logger.info(cleanContent);
+
   return (
-    <div
-      className="blog__post-info__content"
-      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
-    />
+    <div className="blog__post-info__content" dangerouslySetInnerHTML={{ __html: cleanContent }} />
   );
 }
 

8f224df42aeafb57501ee45670f2fbe72da38493
fkurusu7 <coding.fcv@gmail.com>
2025-03-25 22:10:13 -0600

    add link to see a post in admin blog, fix styles in posts table

--- src/features/blog_admin/PostsTable.tsx
+++ src/features/blog_admin/PostsTable.tsx
@@ -1,7 +1,9 @@
 import { HiOutlineXCircle, HiOutlineCheckCircle } from 'react-icons/hi2';
+import { RiImageLine } from 'react-icons/ri';
 import { useBlogPosts } from '../../hooks/useBlogPosts';
-import { formatDateSimple } from '../../utils/helpers';
+import { formatShortDate } from '../../utils/helpers';
 import ButtonActions from './ButtonActions';
+import { Link } from 'react-router-dom';
 
 function PostsTable() {
   const { posts, isLoadingPosts, error, refetch } = useBlogPosts();
@@ -17,24 +19,34 @@ function PostsTable() {
         <div>Post</div>
         <div>Tags</div>
         <div>Date</div>
-        <div>Published</div>
+        <div className="blog-table__header-published">Published</div>
         <div></div>
       </div>
 
       {isLoadingPosts ? (
-        <p>Loading...</p>
+        <div className="blog-table__body-row">
+          <p>Loading...</p>
+        </div>
       ) : error ? (
-        <p>{error}</p>
+        <div className="blog-table__body-row">
+          <p>{error}</p>
+        </div>
       ) : (
         posts.map((post) => {
           return (
             <div className="blog-table__body-row" key={post.title}>
-              <img src={post.banner} alt="..." className="blog-table__body-row_img" />
-              <div className="blog-table__body-row_post">{post.title}</div>
+              {post.banner ? (
+                <img src={post.banner} alt="..." className="blog-table__body-row_img" />
+              ) : (
+                <RiImageLine className="blog-table__body-row_svg" />
+              )}
+              <Link to={`/blog/posts/${post.slug}`} className="blog-table__body-row_post">
+                {post.title}
+              </Link>
               <div className="blog-table__body-row_tags">
                 {post.tags.map((tag) => tag.name).join(', ')}
               </div>
-              <div className="blog-table__body-row_date">{formatDateSimple(post.createdAt)}</div>
+              <div className="blog-table__body-row_date">{formatShortDate(post.createdAt)}</div>
               <div
                 className={`blog-table__body-row_draft ${post.draft ? 'blog-table__body-row_draft-false' : 'blog-table__body-row_draft-true'}`}
               >

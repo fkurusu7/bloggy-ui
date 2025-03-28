a83824e2017b0df61595645b30b6c644dcbc18bb
fkurusu7 <coding.fcv@gmail.com>
2025-03-27 22:13:03 -0600

    add miple responsiveness to blog admin

--- src/features/blog_admin/BlogAdminSidebar.tsx
+++ src/features/blog_admin/BlogAdminSidebar.tsx
@@ -1,37 +1,49 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { HiOutlineUserCircle } from 'react-icons/hi2';
 import { HiOutlineCog6Tooth } from 'react-icons/hi2';
 import { HiOutlineNewspaper } from 'react-icons/hi2';
+import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
 
 function BlogAdminSidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSideBar = () => setIsSidebarOpen(!isSidebarOpen);
+
   return (
-    <aside className="sidebar-admin">
-      <Link to={'/'} className="sidebar-admin__logo">
-        ¯\_(ツ)_/¯
-      </Link>
-      <nav className="sidebar-admin__nav">
-        <ul className="sidebar-admin__ul">
-          <li className="sidebar-admin__li">
-            <NavLink to={'blog/admin'} className="sidebar-admin__link">
-              <HiOutlineNewspaper />
-              <span>Posts</span>
-            </NavLink>
-          </li>
-          <li className="sidebar-admin__li">
-            <NavLink to={'blog/tmp'} className="sidebar-admin__link">
-              <HiOutlineUserCircle />
-              <span>User</span>
-            </NavLink>
-          </li>
-          <li className="sidebar-admin__li">
-            <NavLink to={'blog/admin'} className="sidebar-admin__link">
-              <HiOutlineCog6Tooth />
-              <span>Settings</span>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </aside>
+    <>
+      {/* Mobile Menu Toggle Button */}
+      <button className="sidebar-toggle" onClick={toggleSideBar}>
+        {isSidebarOpen ? <HiOutlineX /> : <HiOutlineMenu />}
+      </button>
+      <aside className={`sidebar-admin ${isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
+        <Link to={'/'} className="sidebar-admin__logo">
+          ¯\_(ツ)_/¯
+        </Link>
+        <nav className="sidebar-admin__nav">
+          <ul className="sidebar-admin__ul">
+            <li className="sidebar-admin__li">
+              <NavLink to={'blog/admin'} className="sidebar-admin__link">
+                <HiOutlineNewspaper />
+                <span>Posts</span>
+              </NavLink>
+            </li>
+            <li className="sidebar-admin__li">
+              <NavLink to={'blog/tmp'} className="sidebar-admin__link">
+                <HiOutlineUserCircle />
+                <span>User</span>
+              </NavLink>
+            </li>
+            <li className="sidebar-admin__li">
+              <NavLink to={'blog/admin'} className="sidebar-admin__link">
+                <HiOutlineCog6Tooth />
+                <span>Settings</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 }
 
